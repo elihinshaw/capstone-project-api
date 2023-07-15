0b30b409b8164d9b98e5e8d39ce11d4717e12beb
Eli <eghinshaw@gmail.com>
2023-07-14 20:20:37 -0500

    added page functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
 class MoviesController < ApplicationController
   def index
-    response = HTTP.auth("Bearer #{ENV["MOVIE_ACCESS_TOKEN"]}").get("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=%2C")
+    page = params[:page] || 1
+
+    response = HTTP.auth("Bearer #{ENV["MOVIE_ACCESS_TOKEN"]}").get("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=#{page}&sort_by=popularity.desc&with_genres=%2C")
     movie_data = response.parse(:json)
     movies = movie_data["results"]
 
